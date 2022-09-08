2ada5d3a78120f9a2997779390aa64388f2bf438
Mrbabet <108229726+Mrbabet@users.noreply.github.com>
2022-09-08 19:25:33 +0200

    Add files via upload

--- about.js
+++ about.js
@@ -2,6 +2,7 @@ const menuBtn = document.querySelector(".burger-bars");
 const hiddenMenu = document.querySelector(".nav-mobile");
 const aboutShade = document.querySelector(".about");
 const overflow = document.querySelector("body");
+const arrowBtn = document.querySelector(".arrow-btn");
 
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
@@ -18,4 +19,22 @@ menuBtn.addEventListener("click", function () {
   hiddenMenu.classList.toggle("active");
   aboutShade.classList.toggle("active");
   overflow.classList.toggle("active");
+});
+
+// Do opracowania jak zrobić, żeby poszczególne buttony aktywowały poszczególny div
+let arrow = false;
+arrowBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    arrowBtn.classList.add("active");
+    arrowOpen = true;
+  } else {
+    arrowBtn.classList.remove("active");
+    arrowOpen = false;
+  }
+});
+arrowBtn.addEventListener("click", function () {
+  const showP = document.querySelectorAll(".toggle");
+  for (let i = 0; i < showP.length; i++) {
+    showP[i].classList.toggle("active");
+  }
 });
